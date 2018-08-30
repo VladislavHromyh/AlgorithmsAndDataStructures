ff2e6fb828ecbfd4da01514f65179b0ad216dd52
Vladislav Hromyh <hromyhvladislav@gmail.com>
2018-08-30 10:18:28 +0300

    Added selection sorting algorithm

---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/SortingAlgorithms/SelectionSorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms {
+
+	internal static class SelectionnSorting {
+		public static void Sort(int[] elements) {
+			int sortedElementsCount = 0;
+
+			for (int i = 0; i < elements.Length; i++) {
+				int minValue = Int32.MaxValue;
+				int minValueIndex = 0;
+				for (int j = sortedElementsCount; j < elements.Length; j++) {
+					if (elements[j] < minValue) {
+						minValue = elements[j];
+						minValueIndex = j;
+					}
+				}
+				int valueToReplace = elements[sortedElementsCount];
+				if (valueToReplace == minValue) {
+					sortedElementsCount++;
+					continue;
+				}
+				elements[sortedElementsCount] = minValue;
+				elements[minValueIndex] = valueToReplace;
+				sortedElementsCount++;
+			}
+		}
+	}
+}
---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class Utils {
+	public static void DebugElements(object[] elements) {
+		foreach(var element in elements) {
+			Console.WriteLine(element);
+		}
+	}
+
+	public static void DebugElements(int[] elements) {
+		foreach (var element in elements) {
+			Console.WriteLine(element);
+		}
+	}
+
+	public static void DebugElements(string[] elements) {
+		foreach (var element in elements) {
+			Console.WriteLine(element);
+		}
+	}
+}
