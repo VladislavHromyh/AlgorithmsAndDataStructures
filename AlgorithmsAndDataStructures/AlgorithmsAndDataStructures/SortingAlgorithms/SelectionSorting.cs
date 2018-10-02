9398136dc51a6c88c49aace0e196837a170198ad
Vladislav Hromyh <hromyhvladislav@gmail.com>
2018-10-03 01:05:36 +0300

    after import at new vs

--- AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/SortingAlgorithms/SelectionSorting.cs
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/SortingAlgorithms/SelectionSorting.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SortingAlgorithms {
+﻿namespace SortingAlgorithms {
 
 	internal static class SelectionnSorting {
 		public static void Sort(int[] elements) {
 			int sortedElementsCount = 0;
 
 			for (int i = 0; i < elements.Length; i++) {
-				int minValue = Int32.MaxValue;
+				int minValue = int.MaxValue;
 				int minValueIndex = 0;
 				for (int j = sortedElementsCount; j < elements.Length; j++) {
 					if (elements[j] < minValue) {
